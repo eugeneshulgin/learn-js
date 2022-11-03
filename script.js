cb18d73c5b1b1d0c26e8673fa3820baf831a4bb8
Eugene Shulgin <eugene.shulgin@mail.ru>
2022-11-03 18:11:53 +0300

    test 2

--- script.js
+++ script.js
@@ -17,6 +17,8 @@
 // 	}
 // }
 
+//////////////////////////////////////////////////////////////////////
+
 // 			2 пример switch case
 
 // let user = document.querySelector('.age');
@@ -35,15 +37,34 @@
 // 			console.log('Добро пожаловать!');
 // 	}
 // }
-//////// test ////
-let browser = 'Firsdsd';
-if(browser === 'Edge'){
-	console.log( "You've got the Edge!");
-}else if (browser === 'Chrome' ||
-browser === 'Firefox' ||
-browser === 'Safari' ||
-browser === 'Opera') {
-	console.log('Okay we support these browsers too');
-} else {
-	console.log('We hope that this page looks ok!');
-}
+//////////////////////////////////////////////////////////////////////
+
+////////						 test 
+
+// let browser = 'Firsdsd';
+// if(browser === 'Edge'){
+// 	console.log( "You've got the Edge!");
+// }else if (browser === 'Chrome' ||
+// browser === 'Firefox' ||
+// browser === 'Safari' ||
+// browser === 'Opera') {
+// 	console.log('Okay we support these browsers too');
+// } else {
+// 	console.log('We hope that this page looks ok!');
+// }
+
+////////						 test 2
+
+// let number = 3;
+// switch (number) {
+// 	case 0:
+// 		console.log('Вы ввели число 0');
+// 		break;
+// 	case 1:
+// 		console.log('Вы ввели число 1');
+// 		break;
+// 	case 2:
+// 	case 3:
+// 		console.log('Вы ввели число 2, а может и 3');
+// 		break;
+// }
