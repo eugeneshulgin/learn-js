edecb4a2e6049a38251bac8e1e28aa0c9904ac32
Eugene Shulgin <eugene.shulgin@mail.ru>
2022-11-03 18:05:12 +0300

    test

--- script.js
+++ script.js
@@ -19,19 +19,31 @@
 
 // 			2 пример switch case
 
-let user = document.querySelector('.age');
-let button = document.querySelector('.click');
+// let user = document.querySelector('.age');
+// let button = document.querySelector('.click');
 
-button.onclick = function () {
-	let userYear = +user.value; 
-	switch (userYear) {
-		case 17:
-			console.log('Приходи через год!');
-			break;
-		case 16:
-			console.log('Ты ещё не готов!');
-			break;		
-		default:
-			console.log('Добро пожаловать!');
-	}
+// button.onclick = function () {
+// 	let userYear = +user.value; 
+// 	switch (userYear) {
+// 		case 17:
+// 			console.log('Приходи через год!');
+// 			break;
+// 		case 16:
+// 			console.log('Ты ещё не готов!');
+// 			break;		
+// 		default:
+// 			console.log('Добро пожаловать!');
+// 	}
+// }
+//////// test ////
+let browser = 'Firsdsd';
+if(browser === 'Edge'){
+	console.log( "You've got the Edge!");
+}else if (browser === 'Chrome' ||
+browser === 'Firefox' ||
+browser === 'Safari' ||
+browser === 'Opera') {
+	console.log('Okay we support these browsers too');
+} else {
+	console.log('We hope that this page looks ok!');
 }
